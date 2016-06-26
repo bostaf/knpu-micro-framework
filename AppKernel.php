f8ea3111ded43bf1b4945c0602ddbe5d65718ba9
Grzegorz Szaliński <grzegorz.szalinski@gmail.com>
2016-06-26 18:56:23 +0200

    Add front controller

---
+++ AppKernel.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * User: bostaf
+ * Date: 26.06.16
+ * Time: 18:43
+ */
+
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Config\Loader\LoaderInterface;
+
+class AppKernel extends Kernel
+{
+    public function registerBundles()
+    {
+        $bundles = array(
+            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new Symfony\Bundle\TwigBundle\TwigBundle(),
+            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle()
+        );
+
+        return $bundles;
+    }
+
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $loader->load(__DIR__ . '/config/config.yml');
+    }
+
+}
---
+++ web/index.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * User: bostaf
+ * Date: 26.06.16
+ * Time: 18:48
+ */
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Debug\Debug;
+
+// If you don't want to setup permissions the proper way, just uncomment the following PHP line
+// read http://symfony.com/doc/current/book/installation.html#checking-symfony-application-configuration-and-setup
+// for more information
+umask(0000);
+
+$env = 'dev';
+$debug = true;
+
+$loader = require __DIR__.'/../vendor/autoload.php';
+require_once __DIR__ . '/../AppKernel.php';
+
+if ($debug) {
+    Debug::enable();
+}
+
+$kernel = new AppKernel($env, $debug);
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
